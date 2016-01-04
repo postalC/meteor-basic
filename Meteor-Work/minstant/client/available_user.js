7e555829b88890287822eabe6e02037ff5607d8a
PostalC <chai.wei.tah@gmail.com>
2016-01-04 20:42:06 +0800

    restructure template

---
+++ Meteor-Work/minstant/client/available_user.js
@@ -0,0 +1,24 @@
+/**
+ * Meteor Client Available User Template JS
+ */
+Template.available_user_list.helpers({
+  users: function() {
+    return Meteor.users.find();
+  }
+})
+
+Template.available_user.helpers({
+  getUsername: function(userId) {
+    user = Meteor.users.findOne({
+      _id: userId
+    });
+    return user.profile.username;
+  },
+  isMyUser: function(userId) {
+    if (userId == Meteor.userId()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+})
