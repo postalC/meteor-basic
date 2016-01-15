5190b187c50edca11033fd92817fc9c181fe32bb
Chai WeiTah <wei.tah.chai@sap.com>
2016-01-15 15:53:54 +0800

    c3-w4-beta-002

--- Meteor-Work/siteace/server/methods.js
+++ Meteor-Work/siteace/server/methods.js
@@ -41,6 +41,23 @@ Meteor.methods({
       $text: {
         $search: searchValue
       }
+    },{
+      // `fields` is where we can add MongoDB projections. Here we're causing
+      // each document published to include a property named `score`, which
+      // contains the document's search rank, a numerical value, with more
+      // relevant documents having a higher score.
+      fields: {
+        score: {
+          $meta: "textScore"
+        }
+      },
+      // This indicates that we wish the publication to be sorted by the
+      // `score` property specified in the projection fields above.
+      sort: {
+        score: {
+          $meta: "textScore"
+        }
+      }
     }).fetch();
     //console.log("Search Result: ", cursor);
     return cursor;
