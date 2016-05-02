86cd738394007c05d6919ceadca92a6f73b3be2a
PostalC <chai.wei.tah@gmail.com>
2016-05-02 13:59:51 +0800

    drawing App test

--- Meteor-Work/drawingApp/server/serverSide.js
+++ Meteor-Work/drawingApp/server/serverSide.js
@@ -1,16 +1,15 @@
 points = new Meteor.Collection('pointsCollection');
 
 Meteor.startup(function() {
-if (!points.findOne()) {
-      points.insert(
-      	{w: 1}
-      	);
-    }
-});
-
-Meteor.methods({
-  'clear': function () {
-    points.remove({});
+  if (!points.findOne()) {
+    points.insert({
+      w: 1
+    });
   }
 });
 
+Meteor.methods({
+  'clear': function() {
+    points.remove({});
+  }
+});
